092caffb92dc3d6436fe80eb4ea8077e9518df0a
Trisha Patel <trisha@curbie.ca>
2020-06-12 22:00:23 -0600

    Added react dnd beautiful

---
+++ src/components/todoTask/listTask.js
@@ -0,0 +1,3 @@
+import taskDetail from "./taskStatus.js";
+import dragEndresult from "./dragEndresult.js";
+import { DragDropContext } from "react-beautiful-dnd";
---
+++ src/components/todoTask/taskStatus.js
@@ -0,0 +1,22 @@
+var taskStatus = {
+  titles: {},
+  columns: {
+    "To-Do": {
+      id: "To-Do",
+      title: "To-Do",
+      titleIds: []
+    },
+    "In-Progress": {
+      id: "In-Progress",
+      title: "In-Progress",
+      titleIds: []
+    },
+    Completed: {
+      id: "Completed",
+      title: "Completed",
+      titleIds: []
+    }
+  },
+  columnsort: ["To-Do", "In-Progress", "Completed"]
+};
+export default taskStatus;
