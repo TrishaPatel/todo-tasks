dd17e0161bb92f753be081430e20bf6394b45255
Trisha Patel <trisha@curbie.ca>
2020-06-14 19:45:49 -0600

    Client app separated

--- Procfile
+++
@@ -1,2 +0,0 @@
-api: rails s -p 3000
-web: cd client && npm start
--- app/models/todo.rb
+++
@@ -1,6 +0,0 @@
-class Todo < ApplicationRecord
-  # validation
-  validates_presence_of :status
-  validates :description, presence: true
-  validates_uniqueness_of :description, case_sensitive: false
-end
